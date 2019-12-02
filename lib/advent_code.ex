f52fd7cb68ad4ff5df9022bd97b8f133a7e64bf2
Pedro Tanaka <pedro.stanaka@gmail.com>
2019-12-02 11:11:36 +0100

    Initial commit. Mix project skeleton

---
+++ lib/advent_code.ex
@@ -0,0 +1,18 @@
+defmodule AdventCode do
+  @moduledoc """
+  Documentation for AdventCode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventCode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/rocket_equation.ex
@@ -0,0 +1,3 @@
+defmodule AdventCode.RocketEquation do
+  
+end
